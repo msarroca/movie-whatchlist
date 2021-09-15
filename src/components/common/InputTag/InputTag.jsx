15ff977525612ff23c502d0cf71c8dd34dab6ea7
msarroca <m.sarroca@gmail.com>
2021-09-15 22:37:45 +0200

    feat: create tag input component

---
+++ src/components/common/InputTag/InputTag.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const InputTag = ({
+  placeholder, required, defaultTags, selectedTags,
+}) => {
+  const [tags, setTags] = useState(defaultTags);
+  const [input, setInput] = useState("");
+  const removeTags = (event, indexToRemove) => {
+    event.preventDefault();
+    const newTags = tags.filter((_, index) => index !== indexToRemove);
+    setTags(newTags);
+    selectedTags(newTags);
+  };
+  const onChangeGenres = (event) => {
+    const { value } = event.target;
+    setInput(value);
+  };
+
+  const addTags = (event) => {
+    event.preventDefault();
+    if (event.target.value !== "") {
+      setInput(event.target.value);
+      setTags([...tags, event.target.value]);
+      selectedTags([...tags, event.target.value]);
+      setInput("");
+    }
+  };
+
+  return (
+    <div className="tags-input">
+      <ul id="tags">
+        {tags.map((tag, index) => (
+          <li key={tag} className="tag">
+            <span className="tag-title">{tag}</span>
+            <button
+              className="tag-close-icon"
+              id={index}
+              onClick={(e) => removeTags(e, index)}
+              type="button"
+            >
+              x
+            </button>
+          </li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        onKeyDown={(event) => (event.key === "Enter" ? addTags(event) : null)}
+        onChange={onChangeGenres}
+        value={input}
+        placeholder={placeholder}
+        required={required}
+      />
+    </div>
+  );
+};
+
+InputTag.propTypes = {
+  required: PropTypes.bool,
+  placeholder: PropTypes.string,
+  defaultTags: PropTypes.arrayOf(PropTypes.string),
+  selectedTags: PropTypes.func,
+};
+
+InputTag.defaultProps = {
+  required: false,
+  placeholder: null,
+  defaultTags: [],
+  selectedTags: null,
+};
+
+export default InputTag;
