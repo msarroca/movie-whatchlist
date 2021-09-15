9b30f5e80fdff48d6ae11765146ef73f48a4ed74
msarroca <m.sarroca@gmail.com>
2021-09-15 22:37:30 +0200

    feat: create input component

---
+++ src/components/common/Input/Input.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Input = ({
+  value, placeholder, type, onChange, required,
+}) => (
+
+  <input
+    className="input-text"
+    value={value}
+    onChange={onChange}
+    placeholder={placeholder}
+    type={type}
+    required={required}
+  />
+);
+
+Input.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  required: PropTypes.bool,
+  placeholder: PropTypes.string,
+  type: PropTypes.string,
+
+};
+
+Input.defaultProps = {
+  value: null,
+  onChange: null,
+  required: false,
+  placeholder: null,
+  type: "text",
+};
+
+export default Input;
