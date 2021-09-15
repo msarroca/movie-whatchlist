f2c271b3a205b0d8f70d37115c265fb234f97484
msarroca <m.sarroca@gmail.com>
2021-09-15 22:41:36 +0200

    feat: create home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Form from "../components/home/Form";
+
+const Home = () => (
+  <div className="home-container">
+    <Form />
+  </div>
+
+);
+
+export default Home;
