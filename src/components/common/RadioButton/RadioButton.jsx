9a24aa37c4db08708bd5e2590c1ccedf7322ca63
msarroca <m.sarroca@gmail.com>
2021-09-16 19:45:53 +0200

    refactor: add radio buttons props

--- src/components/common/RadioButton/RadioButton.jsx
+++ src/components/common/RadioButton/RadioButton.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Input = ({
-  value, text, isSelected, onChange, required, id,
+  value, text, isSelected, onChange, required, id, name,
 }) => (
   <div className="RadioButton">
-    <input id={id} type="radio" onChange={onChange} value={value} name="test" checked={isSelected} required={required} />
+    <input id={id} type="radio" onChange={onChange} value={value} name={name} checked={isSelected} required={required} />
     <label htmlFor={id}>{text}</label>
   </div>
 );
 
 Input.propTypes = {
+  name: PropTypes.string,
   value: PropTypes.string,
   onChange: PropTypes.func,
   required: PropTypes.bool,
@@ -24,6 +25,7 @@ Input.defaultProps = {
   onChange: null,
   required: false,
   text: "",
+  name: "",
   isSelected: false,
   id: "",
 };
