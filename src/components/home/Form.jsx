38d19c6480a8aee35a992d94f47e3e8f49219b91
msarroca <m.sarroca@gmail.com>
2021-09-16 15:44:34 +0200

    feat: add form styles

--- src/components/home/Form.jsx
+++ src/components/home/Form.jsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import Input from "../common/Input/Input";
 import InputTag from "../common/InputTag/InputTag";
 import Button from "../common/Button/Button";
+import "./form.scss";
 
 const Form = () => {
   const [movieName, setMovieName] = useState("");
@@ -13,16 +14,18 @@ const Form = () => {
   };
 
   return (
-    <form className="form-container">
-      <label htmlFor="title">
-        Movie title:
-        <Input value={movieName} placeholder="Insert the movie's title" type="text" onChange={(event) => setMovieName(event.target.value)} required />
-      </label>
-      <label>
-        Movie genres:
-        <InputTag selectedTags={selectedTags} placeholder="Insert a genre and enter to save" required />
-      </label>
-      <Button type="submit" className="simple-button" text="Add movie" />
+    <form className="form">
+      <div className="form-container">
+        <label className="form-container__label" htmlFor="title">
+          Movie title
+          <Input value={movieName} placeholder="Insert the movie's title and click enter to save" type="text" onChange={(event) => setMovieName(event.target.value)} required />
+        </label>
+        <label className="form-container__label" htmlFor="genres">
+          Movie genres
+          <InputTag selectedTags={selectedTags} placeholder="Insert a genre and click enter to save" required />
+        </label>
+        <Button type="submit" className="simple-button" text="Add movie" />
+      </div>
     </form>
   );
 };
