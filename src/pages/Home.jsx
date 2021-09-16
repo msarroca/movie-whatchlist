020d0ba7a645d5b7a279075f26fc4e52f95de602
msarroca <m.sarroca@gmail.com>
2021-09-16 19:47:39 +0200

    feat: add list to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Form from "../components/home/Form";
+import List from "../components/home/List";
+
 import "./home.scss";
 
 const Home = () => (
   <div className="home-container">
     <Form />
+    <List />
   </div>
 
 );
