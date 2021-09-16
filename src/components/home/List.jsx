56e334376a8735c3046a2f5a0c6da5d5dd14f508
msarroca <m.sarroca@gmail.com>
2021-09-16 19:46:34 +0200

    feat: create list component

--- src/components/home/List.jsx
+++ src/components/home/List.jsx
@@ -1,1 +1,27 @@
-// TODO: create list component
+/* eslint-disable react/jsx-props-no-spreading */
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Card from "./Card";
+import { updateMovie, deleteMovie } from "../../store/slices/movies/slice";
+
+import "./form.scss";
+
+const List = () => {
+  const dispatch = useDispatch();
+  const { movies } = useSelector((state) => state.movies);
+
+  return (
+    <section className="list-container">
+      {movies && movies.map((movie, index) => (
+        <Card
+          {...movie}
+          updateMovie={(name, value) => dispatch(updateMovie({ index, name, value }))}
+          deleteMovie={() => dispatch(deleteMovie(index))}
+        />
+      ))}
+
+    </section>
+  );
+};
+
+export default List;
