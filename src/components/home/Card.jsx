8b45b99c3ad5e51965215b9d423497df09ba94a4
msarroca <m.sarroca@gmail.com>
2021-09-18 16:55:25 +0200

    refactor: card component

--- src/components/home/Card.jsx
+++ src/components/home/Card.jsx
@@ -2,21 +2,29 @@ import React from "react";
 import PropTypes from "prop-types";
 import CheckBox from "../common/Checkbox/Checkbox";
 import Button from "../common/Button/Button";
+import "../../assets/movieImg.png";
+import "./card.scss";
 
 const Card = ({
-  name, genres, watched, updateMovie, deleteMovie,
+  image, name, genres, watched, updateMovie, deleteMovie,
 }) => (
   <article className="card">
-    <p>{name}</p>
-    <ul>
-      {genres.map((genre) => <li>{genre}</li>)}
-    </ul>
-    <CheckBox isChecked={watched} onChange={() => updateMovie("watched", !watched)} value={watched} name={name} text="Watched?" />
-    <Button className="delete-button" text="delete" type="button" handleClick={deleteMovie} />
+    <div className="container-image">
+      <img className="image" src={image} alt="movie" />
+    </div>
+    <div className="container-info">
+      <p className="container-info__title">{name}</p>
+      <ul className="container-info__genres">
+        {genres.map((genre) => <li className="tag" key={genre}>{genre}</li>)}
+      </ul>
+      <CheckBox className="container-info__checkbox" isChecked={watched} onChange={() => updateMovie("watched", !watched)} value={watched} name={name} text="watched?" />
+      <Button className="delete-button" text="delete" type="button" handleClick={deleteMovie} />
+    </div>
   </article>
 );
 
 Card.propTypes = {
+  image: PropTypes.string,
   name: PropTypes.string,
   genres: PropTypes.arrayOf(PropTypes.string),
   watched: PropTypes.bool,
@@ -26,6 +34,7 @@ Card.propTypes = {
 };
 
 Card.defaultProps = {
+  image: "",
   name: "",
   genres: [],
   watched: false,
