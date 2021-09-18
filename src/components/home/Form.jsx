535ebf4c4ea0660f57a4a2c999af5558c138f09c
msarroca <m.sarroca@gmail.com>
2021-09-18 16:57:00 +0200

    refactor: form component

--- src/components/home/Form.jsx
+++ src/components/home/Form.jsx
@@ -1,21 +1,33 @@
 /* eslint-disable no-console */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Input from "../common/Input/Input";
 import InputTag from "../common/InputTag/InputTag";
 import Button from "../common/Button/Button";
-import "./form.scss";
 import { createMovie } from "../../store/slices/movies/actions";
+import imageMovie from "../../assets/movieImg.png";
+import "./form.scss";
 
 const Form = () => {
   const dispatch = useDispatch();
   const [selectedTags, setSelectedTags] = useState([]);
+  const [inputName, setInputName] = useState("");
+
+  const { isLoading, error } = useSelector((state) => state.movies);
+
+  useEffect(() => {
+    if (!isLoading && !error) {
+      setSelectedTags([]);
+      setInputName("");
+    }
+  }, [isLoading, error]);
 
   const onAddNewMovie = (event) => {
     event.preventDefault();
     const movie = {
-      name: event.target.name.value,
+      image: imageMovie,
+      name: inputName,
       genres: selectedTags,
       watched: false,
     };
@@ -27,11 +39,11 @@ const Form = () => {
       <div className="form-container">
         <label className="form-container__label" htmlFor="title">
           Movie title
-          <Input name="name" placeholder="Insert the movie's title and click enter to save" type="text" required />
+          <Input onChange={(e) => setInputName(e.target.value)} value={inputName} name="name" placeholder="Insert the movie's title and click enter to save" type="text" required />
         </label>
         <label className="form-container__label" htmlFor="genres">
           Movie genres
-          <InputTag selectedTags={setSelectedTags} placeholder="Insert a genre and click enter to save" />
+          <InputTag setTags={setSelectedTags} placeholder="Insert a genre and click enter to save" tags={selectedTags} />
         </label>
         <Button type="submit" className="simple-button" text="Add movie" />
       </div>
