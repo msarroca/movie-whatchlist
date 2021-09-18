d99f22d4517d8bea1aee28ea4b37c4d2940be226
msarroca <m.sarroca@gmail.com>
2021-09-18 19:40:37 +0200

    feat: add edit functionality

--- src/components/home/Card.jsx
+++ src/components/home/Card.jsx
@@ -1,27 +1,73 @@
-import React from "react";
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import CheckBox from "../common/Checkbox/Checkbox";
 import Button from "../common/Button/Button";
-import "../../assets/movieImg.png";
+import Input from "../common/Input/Input";
+import Edit from "../../assets/edit.png";
+
 import "./card.scss";
 
 const Card = ({
   image, name, genres, watched, setUpdateMovie, setDeleteMovie,
-}) => (
-  <article className="card">
-    <div className="container-image">
-      <img className="image" src={image} alt="movie" />
-    </div>
-    <div className="container-info">
-      <p className="container-info__title">{name}</p>
-      <ul className="container-info__genres">
-        {genres.map((genre) => <li className="tag" key={genre}>{genre}</li>)}
-      </ul>
-      <CheckBox className="container-info__checkbox" isChecked={watched} onChange={() => setUpdateMovie("watched", !watched)} value={watched} name={name} text="watched?" />
-      <Button className="delete-button" text="delete" type="button" handleClick={setDeleteMovie} />
-    </div>
-  </article>
-);
+}) => {
+  const [isEdit, setEdit] = useState(false);
+  const [inputValue, setInputValue] = useState(name);
+
+  return (
+
+    <article className="card">
+      <div className="container-image">
+        <img className="image" src={image} alt="movie" />
+      </div>
+      <div className="container-info">
+
+        {isEdit
+          ? (
+            <>
+              <Input
+                placeholder="Enter new name"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                name="editName"
+              />
+              <Button
+                className="edit-button"
+                text="Save"
+                type="button"
+                handleClick={() => setUpdateMovie("name", inputValue)}
+              />
+            </>
+          ) : (
+            <>
+              <p className="container-info__title">{name}</p>
+              <div onClick={() => setEdit(!isEdit)}>
+                <img className="image" src={Edit} alt="edit" width="20px" />
+              </div>
+            </>
+          )}
+        <ul className="container-info__genres">
+          {genres.map((genre) => <li className="tag" key={genre}>{genre}</li>)}
+        </ul>
+        <CheckBox
+          className="container-info__checkbox"
+          isChecked={watched}
+          onChange={() => setUpdateMovie("watched", !watched)}
+          value={watched}
+          name={name}
+          text="watched?"
+        />
+        <Button
+          className="delete-button"
+          text="delete"
+          type="button"
+          handleClick={setDeleteMovie}
+        />
+      </div>
+    </article>
+  );
+};
 
 Card.propTypes = {
   image: PropTypes.string,
