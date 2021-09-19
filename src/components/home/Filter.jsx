416aecbc1164a914eca2ae15864195fa0cdbab1b
msarroca <m.sarroca@gmail.com>
2021-09-19 15:19:50 +0200

    feat: add filter styles

--- src/components/home/Filter.jsx
+++ src/components/home/Filter.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from "react";
 import { useHistory } from "react-router-dom";
 import PropTypes from "prop-types";
@@ -5,38 +6,46 @@ import RadioButton from "../common/RadioButton/RadioButton";
 import Input from "../common/Input/Input";
 import Button from "../common/Button/Button";
 import "../../assets/movieImg.png";
-import "./card.scss";
+import "./filter.scss";
 
 const Filter = ({ filter, onChangeFilter }) => {
   const genresFilter = ["Horror", "Romance", "Comedy"];
   const history = useHistory();
 
   return (
-    <section>
-      {genresFilter.map((genre) => (
-        <RadioButton
-          key={genre}
-          value={genre}
-          text={genre}
-          isSelected={genre.toLocaleLowerCase() === filter.genre.toLocaleLowerCase()}
-          onChange={() => history.push(`/genre=${genre.toLocaleLowerCase()}`)}
-          id={genre}
-          name={genre}
+    <section className="container">
+      <div className="filter-container">
+        <div className="radio-buttons-container">
+          {genresFilter.map((genre) => (
+            <RadioButton
+              key={genre}
+              value={genre}
+              text={genre}
+              isSelected={genre.toLocaleLowerCase() === filter.genre.toLocaleLowerCase()}
+              onChange={() => history.push(`/genre=${genre.toLocaleLowerCase()}`)}
+              id={genre}
+              name={genre}
+            />
+          ))}
+        </div>
+        <label className="label-container" htmlFor="name">
+          Search movie:
+          <Input
+            onChange={(e) => onChangeFilter({ ...filter, name: e.target.value })}
+            value={filter.name}
+            name="name"
+            placeholder="Insert the movie's title"
+            type="text"
+            className="label-container__input"
+          />
+        </label>
+        <Button
+          type="button"
+          className="simple-button"
+          text="Reset"
+          handleClick={() => history.push("/")}
         />
-      ))}
-      <Input
-        onChange={(e) => onChangeFilter({ ...filter, name: e.target.value })}
-        value={filter.name}
-        name="name"
-        placeholder="Search"
-        type="text"
-      />
-      <Button
-        type="button"
-        className="simple-button"
-        text="Reset"
-        handleClick={() => history.push("/")}
-      />
+      </div>
     </section>
   );
 };
